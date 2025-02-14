40b8a9473ea251620ec1a4f3d4ce4f1f324ffc84
hanhtx <kpthanh26@gmail.com>
2025-02-14 17:29:39 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,18 @@
-import { motion, useScroll, useTransform } from "framer-motion";
+import ScrollGallery from "./ScrollGallery";
 
-const largeImages = [
-  "https://www.hkt.co.jp/images/index/index_feature_bg1.jpg",
-  "https://www.hkt.co.jp/images/index/index_feature_bg2.jpg",
-  "https://www.hkt.co.jp/images/index/index_feature_bg3.jpg",
-  "https://www.hkt.co.jp/images/index/index_feature_bg4.jpg",
-];
-
-const smallImages = [
-  "https://www.hkt.co.jp/images/index/index_feature_img4.jpg",
-  "https://www.hkt.co.jp/images/index/index_feature_img3.jpg",
-  "https://www.hkt.co.jp/images/index/index_feature_img2.jpg",
-  "https://www.hkt.co.jp/images/index/index_feature_img1.jpg"
-
-];
-
-const ScrollGallery = () => {
-  const { scrollYProgress } = useScroll();
-  const yLarge = useTransform(scrollYProgress, [0, 1], ["0%", "-300%"]);
-  const ySmall = useTransform(scrollYProgress, [1, 0], ["0%", "-310px"]);
+const App = () => {
 
   return (
-    <div className="relative w-full min-h-[400vh]">
-      <motion.div className="fixed inset-0 z-[-1]" style={{ y: yLarge }}>
-        {largeImages.map((src, i) => (
-          <div
-            key={i}
-            className="absolute inset-0 bg-cover bg-center h-screen w-full"
-            style={{ backgroundImage: `url(${src})`, top: `${i * 100}vh` }}
-          />
-        ))}
-      </motion.div>
-      <div className="fixed left-[10%] top-1/2 transform -translate-y-1/2 w-[326px] h-[326px] overflow-hidden shadow-lg">
-        <motion.div className="relative w-full h-full" style={{ y: ySmall }}>
-          {smallImages.reverse().map((src, i) => (
-            <div
-              key={i}
-              className="absolute inset-x-0 w-full h-full bg-cover bg-center"
-              style={{ backgroundImage: `url(${src})`, top: `calc(${i * 100}% + ${i * 10}px)` }}
-            />
-          ))}
-        </motion.div>
+    <>
+      <div className="w-full h-[200vh] bg-[#CACACA] text-center text-2xl pt-20">
+        Phần bên trên
       </div>
-
-    </div>
+      <ScrollGallery />
+      <div className="w-full h-[200vh] bg-[#CACACA] text-center text-2xl pt-20">
+        Phần bên dưới
+      </div>
+    </>
   );
 }
 
-export default ScrollGallery;
+export default App;
